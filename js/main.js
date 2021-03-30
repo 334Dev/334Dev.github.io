bbf5c9598dbffdd8c93ed1de3682b7f8f57a3c39
Anant Shukla <anantshukla1402@gmail.com>
2021-03-31 00:31:55 +0530

    Landing Page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,11 @@
-(function($){
-    // variables
-    elementWidth = $('ul').width(),
-    containerWidth = $('nav').width(),
-    difference = elementWidth-containerWidth,
-    finalWidth = difference * 1.5,
-    element = $('ul');
-    
-    // active on click
-    $('li').on('click', function(){
-      $('li').removeClass('active');
-      $(this).addClass('active');
-    });
-    
-  })(jQuery);
+window.addEventListener('scroll', function(){
+  var header= document.querySelector('header');
+  header.classList.toggle('sticky', window.scrollY > 0);    
+  });
+  
+  function toggleMenu(){
+    var menutoggle = document.querySelector('.toggle');
+    var menu = document.querySelector('.menu');
+    menutoggle.classList.toggle('active');
+    menu.classList.toggle('active') 
+  }
